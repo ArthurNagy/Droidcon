4fd9a176d614ae736a0e577707fa7c7b404bb103
Péter Pandula <ppeter@halcyonmobile.com>
2017-09-01 10:32:41 +0300

    Enable up navigation on the session detail screen.

--- app/src/main/java/com/arthurnagy/droidconberlin/feature/session/SessionDetailActivity.kt
+++ app/src/main/java/com/arthurnagy/droidconberlin/feature/session/SessionDetailActivity.kt
@@ -2,6 +2,10 @@ package com.arthurnagy.droidconberlin.feature.session
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import com.arthurnagy.droidconberlin.R
+import com.arthurnagy.droidconberlin.SessionDetailBinding
 import com.arthurnagy.droidconberlin.architecture.DroidconActivity
 
 class SessionDetailActivity : DroidconActivity() {
@@ -10,5 +14,13 @@ class SessionDetailActivity : DroidconActivity() {
 
         fun getStartIntent(context: Context, sessionId: String): Intent =
                 Intent(context, SessionDetailActivity::class.java).putExtra(SESSION_ID, sessionId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<SessionDetailBinding>(this, R.layout.activity_session_detail)
+        binding.viewModel = getViewModel(SessionDetailViewModel::class.java)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- app/src/main/java/com/arthurnagy/droidconberlin/feature/session/SessionDetailViewModel.kt
+++ app/src/main/java/com/arthurnagy/droidconberlin/feature/session/SessionDetailViewModel.kt
@@ -1,6 +1,10 @@
 package com.arthurnagy.droidconberlin.feature.session
 
+import android.databinding.ObservableField
 import com.arthurnagy.droidconberlin.architecture.viewmodel.DroidconViewModel
+import com.arthurnagy.droidconberlin.model.Session
 import javax.inject.Inject
 
-class SessionDetailViewModel @Inject constructor() : DroidconViewModel()
+class SessionDetailViewModel @Inject constructor() : DroidconViewModel() {
+    val session = ObservableField<Session>()
+}
