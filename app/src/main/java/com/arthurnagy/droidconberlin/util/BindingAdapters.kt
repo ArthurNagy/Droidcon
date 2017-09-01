07b3fee8654d321361d4aa351c5cedd2c26705ab
Arthur Nagy <arthur@halcyonmobile.com>
2017-09-01 11:19:16 +0300

    Change title to Roboto Medium

--- app/src/main/java/com/arthurnagy/droidconberlin/util/BindingAdapters.kt
+++ app/src/main/java/com/arthurnagy/droidconberlin/util/BindingAdapters.kt
@@ -2,6 +2,7 @@ package com.arthurnagy.droidconberlin.util
 
 import android.databinding.BindingAdapter
 import android.support.annotation.DrawableRes
+import android.support.annotation.StyleRes
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -9,6 +10,16 @@ import android.widget.TextView
 @BindingAdapter("android:textStyle")
 fun setTextStyle(textView: TextView, textStyle: Int) {
     textView.setTypeface(textView.typeface, textStyle)
+}
+
+@BindingAdapter("android:textAppearance")
+fun setTextAppearance(textView: TextView, @StyleRes textAppearance: Int) {
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+        textView.setTextAppearance(textAppearance)
+    } else {
+        @Suppress("DEPRECATION")
+        textView.setTextAppearance(textView.context, textAppearance)
+    }
 }
 
 @BindingAdapter("android:src")
