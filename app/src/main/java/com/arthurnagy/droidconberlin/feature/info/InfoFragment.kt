1a5135d567e1658970a4327ea8341eb23c610f62
Péter Pandula <ppeter@halcyonmobile.com>
2017-09-01 10:46:43 +0300

    Open Chrome Custom Tabs for external links on the Info page.

--- app/src/main/java/com/arthurnagy/droidconberlin/feature/info/InfoFragment.kt
+++ app/src/main/java/com/arthurnagy/droidconberlin/feature/info/InfoFragment.kt
@@ -1,13 +1,16 @@
 package com.arthurnagy.droidconberlin.feature.info
 
 import android.databinding.DataBindingUtil
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.arthurnagy.droidconberlin.InfoBinding
 import com.arthurnagy.droidconberlin.R
 import com.arthurnagy.droidconberlin.architecture.DroidconFragment
+import com.arthurnagy.droidconberlin.util.color
 import com.arthurnagy.droidconberlin.util.setupToolbar
 
 class InfoFragment : DroidconFragment() {
@@ -25,6 +28,15 @@ class InfoFragment : DroidconFragment() {
 
         val viewModel = getViewModel(InfoViewModel::class.java)
         binding.viewModel = viewModel
+
+        binding.codeOfConduct.setOnClickListener { openUrl("https://droidcon.de/en/berlin/17/code-conduct") }
+        binding.venue.setOnClickListener { openUrl("https://droidcon.de/en/berlin/17/venue") }
+        binding.viewOnGitHub.setOnClickListener { openUrl("https://github.com/ArthurNagy/DroidconBerlin") }
+        binding.share.setOnClickListener {}
     }
 
+    private fun openUrl(url: String) = CustomTabsIntent.Builder()
+            .setToolbarColor(context.color(R.color.primary))
+            .setShowTitle(true)
+            .build().launchUrl(context, Uri.parse(url))
 }
