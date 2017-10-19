46815ef5a5c04bd6f682c3299d10fffc2351a339
Arthur <arthur.93.nagy@gmail.com>
2017-10-19 17:10:10 +0300

    Remove Observable implementation from DroidconViewModel

--- app/src/main/java/com/arthurnagy/droidconberlin/architecture/viewmodel/DroidconViewModel.kt
+++ app/src/main/java/com/arthurnagy/droidconberlin/architecture/viewmodel/DroidconViewModel.kt
@@ -1,38 +1,12 @@
 package com.arthurnagy.droidconberlin.architecture.viewmodel
 
 import android.arch.lifecycle.ViewModel
-import android.databinding.Observable
-import android.databinding.PropertyChangeRegistry
 import android.support.annotation.CallSuper
 import io.reactivex.disposables.CompositeDisposable
 
-open class DroidconViewModel : ViewModel(), Observable {
+open class DroidconViewModel : ViewModel() {
 
     protected val disposables = CompositeDisposable()
-
-    private val propertyCallbacks by lazy { PropertyChangeRegistry() }
-
-    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback) {
-        propertyCallbacks.remove(callback)
-    }
-
-    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback) {
-        propertyCallbacks.add(callback)
-    }
-
-    /**
-     * Notifies listeners that all properties of this instance have changed.
-     */
-    fun notifyChange() = propertyCallbacks.notifyChange(this, 0)
-
-    /**
-     * Notifies listeners that a specific property has changed. The getter for the property
-     * that changes should be marked with [android.databinding.Bindable] to generate a field in
-     * [com.arthurnagy.androidarchitecturesample.BR] to be used as <code>fieldId</code>.
-     *
-     * @param fieldId The generated BR id for the Bindable field.
-     */
-    fun notifyPropertyChanged(fieldId: Int) = propertyCallbacks.notifyChange(this, fieldId)
 
     @CallSuper
     override fun onCleared() {
